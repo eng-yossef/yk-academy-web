669647f2363a15213f206d4e476d5c5212e6f06e
Youssef Khaled Abdou <yossefkhaled551@gmail.com>
2026-07-24 00:52:03 +0300

    fix: add postinstall prisma generate, exclude prisma from TS, configure serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@prisma/client", "bcryptjs"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
